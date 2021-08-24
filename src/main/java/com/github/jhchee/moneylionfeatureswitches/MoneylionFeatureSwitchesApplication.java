69bc7f46670f172a7ab7302e98b74fa045916502
jhchee <cheehong05090@gmail.com>
2021-08-24 17:59:04 +0800

    initialize project

---
+++ src/main/java/com/github/jhchee/moneylionfeatureswitches/MoneylionFeatureSwitchesApplication.java
@@ -0,0 +1,13 @@
+package com.github.jhchee.moneylionfeatureswitches;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneylionFeatureSwitchesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneylionFeatureSwitchesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/jhchee/moneylionfeatureswitches/MoneylionFeatureSwitchesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.jhchee.moneylionfeatureswitches;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneylionFeatureSwitchesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
