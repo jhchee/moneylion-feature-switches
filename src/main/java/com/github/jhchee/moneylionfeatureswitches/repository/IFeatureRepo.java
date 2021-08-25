65f1c8ccd842c519cf6d189a23ff00425262ace2
jhchee <cheehong05090@gmail.com>
2021-08-25 16:36:11 +0800

    initialize repository to access model

---
+++ src/main/java/com/github/jhchee/moneylionfeatureswitches/repository/IFeatureRepo.java
@@ -0,0 +1,8 @@
+package com.github.jhchee.moneylionfeatureswitches.repository;
+
+import com.github.jhchee.moneylionfeatureswitches.model.Feature;
+import org.springframework.data.repository.CrudRepository;
+
+public interface IFeatureRepo extends CrudRepository<Feature, Integer> {
+
+}
---
+++ src/main/java/com/github/jhchee/moneylionfeatureswitches/repository/IUserRepo.java
@@ -0,0 +1,8 @@
+package com.github.jhchee.moneylionfeatureswitches.repository;
+
+import com.github.jhchee.moneylionfeatureswitches.model.User;
+import org.springframework.data.repository.CrudRepository;
+
+public interface IUserRepo extends CrudRepository<User, Integer> {
+
+}
